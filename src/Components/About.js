81408151b07a9499816280ee85dd69e95343b0af
Ankur Sharma <ankursharma.1611@gmail.com>
2023-07-10 13:31:31 +0530

    About component build

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,21 @@
+import FrontendPhoto from "../Constants/frontend.jpg";
+
 const About = () => {
   return (
-    <div>
-      <h1>About Component</h1>
+    <div className=" flex bg-gray-200 place-content-center h-screen items-center">
+      <div className="flex-wrap items-center box-border h-3/6 w-3/6  bg-white border-black border-2 ">
+        <h1 className="flex-row text-center font-bold text-2xl">About</h1>
+        <p className="p-4 font-bold ">
+          Ankur is a skilled frontend developer with expertise in HTML, CSS, and
+          JavaScript. Proficient in React, he transforms design concepts into
+          user-friendly websites. Detail-oriented and a strong problem solver,
+          Ankur delivers exceptional frontend solutions while collaborating
+          effectively with cross-functional teams.
+        </p>
+        <div className="flex place-content-center shrink h-4/6 w-full items-center">
+          <img src={FrontendPhoto} className="h-3/6"></img>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,14 @@
 const Header = () => {
   return (
     <div className="flex justify-end bg-gray-200 shadow-lg ">
-      <div>
+      <nav>
         <ul className="flex p-4 m-4">
           <li className="hover:cursor-pointer m-4 p-2 font-bold">Home</li>
           <li className="hover:cursor-pointer m-4 p-2 font-bold">About</li>
+          <li className="hover:cursor-pointer m-4 p-2 font-bold">Project</li>
           <li className="hover:cursor-pointer m-4 p-2 font-bold">Contact</li>
         </ul>
-      </div>
+      </nav>
     </div>
   );
 };
